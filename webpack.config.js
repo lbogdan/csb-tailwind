7df7a5e5d240ad17e9ca156cd64db533a1ce6914
Bogdan Luca <luca.bogdan@gmail.com>
2020-09-17 08:31:09 +0000

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const { resolve, join } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const srcPath = resolve(__dirname, 'src');
+
+module.exports = {
+  entry: {
+    index: join(srcPath, 'index.js'),
+  },
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: join(srcPath, 'index.html'),
+    }),
+  ],
+  devServer: {
+    port: 9000,
+  },
+};
